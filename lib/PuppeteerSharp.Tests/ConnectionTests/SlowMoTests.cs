b5dd5aa859825570ef64397e4d2459766e674118
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-10-25 08:17:07 -0300

    SlowMo test

---
+++ lib/PuppeteerSharp.Tests/ConnectionTests/SlowMoTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using PuppeteerSharp.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.ConnectionsTests
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class SlowMoTests : PuppeteerBaseTest
+    {
+        public SlowMoTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task ShouldWork()
+        {
+            var options = TestConstants.DefaultBrowserOptions();
+            options.SlowMo = 100;
+
+            using (var browser = await Puppeteer.LaunchAsync(options, TestConstants.LoggerFactory))
+            using (var page = await browser.NewPageAsync())
+            {
+                await page.GoToAsync(TestConstants.EmptyPage);
+            }
+        }
+    }
+}
